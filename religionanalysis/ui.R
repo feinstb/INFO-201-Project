234e02d5bb38684d96bb87155e4d80ace3037fb0
nyadav18 <nyadav8@uw.edu>
2023-03-07 15:15:35 -0800

    finished summary

--- religionanalysis/ui.R
+++ religionanalysis/ui.R
@@ -76,11 +76,48 @@ fluidPage(
                         ),
                tabPanel("Summary",
                         p((h2(strong("Conclusion")))),
-                          p("A recurring pattern in our data was religiously affiliated people and non-affiliated people tending to disagree more heavily on already contreversal topics.
-                          This included issues such as the death penality and immigration where the two groups had starkly different views while the difference in opinion for topics such as sex educations was almost negligable.The broader implications of these results are that religiously affiliated people and non-affiliated people have fumdentally different beliefs and values in matters that are more grey and less clear as to what is acceptable or right in our current society"),
-                          p("Viewing trends over time as they relate to this topic would be incredibly insightful, when it comes to the general social survey we'd have to merge several large datasets into one to display trends and it would require a lot of time, but it could be an interesting future project."),
-                          p((h5(strong("Data Quality")))),
-                          p("The dataset we used was of reasonable quality with no bias results. However, there were multiple senitive topics in the dataset so it could pontentially impact these groups."))
+                          p("A recurring pattern in our data was religiously 
+                          affiliated people and non-affiliated people tending 
+                          to disagree more heavily on already controversial topics.
+                          This included issues such as the death penality and 
+                          immigration where the two groups had starkly 
+                          different views while the difference in opinion for 
+                          topics such as sex education in grade school was almost negligable. 
+                          The broader implications of these results are that 
+                          religiously affiliated people and non-affiliated 
+                          people have fumdentally different beliefs and values 
+                          in matters that are more grey and less clear as to 
+                          what is acceptable or right in our current society. 
+                          It is also important to note that in a vast majority
+                          of cases religion informs an individual's ethics and 
+                          shapes their political inclinations. It isn't inherently 
+                          a tool for division, but does seem to unintentionally 
+                          create a divide. The rise in the nonreligious population,
+                          at least in the U.S., creates the potential for a shift 
+                          in attitudes regarding many important political and 
+                          cultural topics, which could have a far-reaching 
+                          impact on the country as a whole."),
+                        p((h2(strong("Future Research")))),
+                          p("Viewing trends over time as they relate to this 
+                            topic would be incredibly insightful, when it comes 
+                            to the general social survey we'd have to merge several 
+                            large datasets into one to display trends and it would 
+                            require a lot of time, but it could be an interesting 
+                            future project."),
+                          p((h2(strong("Data Quality")))),
+                          p("The dataset we used was of reasonable quality. There 
+                          wasn't any clear signs of bias in the original survey and 
+                          when it came to choosing variables we tried to choose 
+                          the survey questions that had the least potential to 
+                          draw in biased results. There were 710 variables in 
+                          the original dataset and so some of the questions, with 
+                          the way they were phrased and the terminology used, could 
+                          have unintentially biased the responses of those surveyed.
+                          There were multiple sensitive topics in the dataset, since 
+                          the survey gathered data on such a vast amount of things and 
+                          aimed to develop an understanding on American opinions in 
+                          general, but for the most part the questions simply bring 
+                          awareness to current attitudes without asserting judgement."))
                           
     )
 
